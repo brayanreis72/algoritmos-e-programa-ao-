7265fe15c6b49985654f72bf210dbc90e0f2471e
Brayanreis09 <brayanreis2001@gmail.com>
2026-09-01 20:58:32 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    double A;
+    double B;
+    double N1;
+    double N2;
+
+    System.out.println("Digite o  valor de A:");
+    A = input.nextDouble();
+
+    System.out.println("Digite o valor de B:");
+    B = input.nextDouble();
+
+    N1 = A;
+    N2 = B;
+    System.out.println("O valor de A é " + " " + N1 + " " + " e o valor de B é " + " " + N2);
+}
