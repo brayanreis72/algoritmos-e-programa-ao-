16da359c3db10b4214218fa8016ca2dbfd72ed48
Brayanreis09 <brayanreis2001@gmail.com>
2026-09-01 21:14:45 -0300

    Add files via upload

---
+++ listaexercicio1jardeu/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public static void main() {
+    Scanner input = new Scanner (System.in);
+    double N1;
+    double dobro;
+    System.out.println("Digite o número:");
+    N1 = input.nextDouble();
+    dobro = N1*2;
+    System.out.println("O dobro do número digitado é:"+ ""+ dobro);
+
+}
